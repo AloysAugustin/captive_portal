5293deebf99c2f4a47be19e0d6266c80192eb6da
Aloys Augustin <aloys.augustin@polytechnique.org>
2016-01-18 18:00:05 +0100

    Remove unused references to wlan1

--- start.sh
+++ start.sh
@@ -6,10 +6,6 @@ tmux -2 new-session -d -s $SESSION
 tmux split-window -h
 tmux select-pane -t 0
 
-tmux send-keys "ifconfig wlan0 hw ether 00:c0:ca:59:e9:d0" C-m
-
-sleep 1
-
 tmux send-keys "hostapd hostapd.conf" C-m
 
 sleep 10
@@ -17,10 +13,6 @@ sleep 10
 tmux select-pane -t 1
 
 tmux send-keys "ifconfig wlan0 10.20.0.1/24 up" C-m
-
-sleep 1
-
-tmux send-keys "ifconfig wlan1 10.21.0.1/24 up" C-m
 
 sleep 1
 
